32f065b56cb8d11fbf6f0eaeb3dd2f889cf55b55
benniphilipp <root@v2202311107229245784.goodsrv.de>
2023-11-24 20:05:00 +0100

    Crate

---
+++ src/src/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=t=1$oekehhkg2fhf8^bjja116n4_5h(4_s=prtrp6q(a0#3&!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
