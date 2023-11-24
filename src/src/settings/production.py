fceb659e33f3336c00bf7cc35b67ed10cfe1614d
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-11-24 20:30:20 +0100

    Database

--- src/src/settings/production.py
+++ src/src/settings/production.py
@@ -2,6 +2,17 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'health4kids',
+        'USER': 'health4kidsuser',
+        'PASSWORD': 'health4kids',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 try:
     from .local import *
 except ImportError:
