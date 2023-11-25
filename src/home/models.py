23bcd3c35210fd9abb599b775c15ba8fc09dcd59
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-11-25 19:06:16 +0100

    Crate Home

--- src/home/models.py
+++ src/home/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
-    pass
+    template = "home/home_page.html"
+    max_count = 1
+    
+    # TODO 
+    # Background Image
+    # Image Left
+    # 
+    
