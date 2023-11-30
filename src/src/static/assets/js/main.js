05049d62a7734641239562d07105bdd95b7a9763
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-11-30 15:28:43 +0100

    Menu Fertig

--- src/src/static/assets/js/main.js
+++ src/src/static/assets/js/main.js
@@ -1,1 +1,21 @@
 import '../../../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const OpenMenu = document.querySelector('#menuOpen');
+    if(OpenMenu){
+        OpenMenu.addEventListener('click', () => {
+            const burgerMenu = document.querySelector('.burger-menu');
+            burgerMenu.classList.toggle('active');
+
+            const mobileMenuDiv = document.querySelector('#mobileMenuItem');
+            mobileMenuDiv.classList.toggle('visible');
+
+            const body = document.querySelector('body');
+            body.classList.toggle('body-fixed');
+
+        })
+    }
+
+});
