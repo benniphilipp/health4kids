e998ee0a728d9b0f54b09981d7ad7f953197db8b
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-12-13 12:09:33 +0100

    Update Image

--- src/src/settings/base.py
+++ src/src/settings/base.py
@@ -169,17 +169,6 @@ MEDIA_URL = "/media/"
 
 WAGTAIL_SITE_NAME = "src"
 
-# Make low-quality but small images
-WAGTAILIMAGES_JPEG_QUALITY = 40
-WAGTAILIMAGES_WEBP_QUALITY = 45
-
-WAGTAILIMAGES_FORMAT_CONVERSIONS = {
-        'avif': 'avif',
-        'bmp': 'jpeg',
-        'webp': 'webp',
-        'png': 'png',
-}
-
 # Search
 # https://docs.wagtail.org/en/stable/topics/search/backends.html
 WAGTAILSEARCH_BACKENDS = {
